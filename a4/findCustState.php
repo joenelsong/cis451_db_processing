8779227808a49d2df3d73cb14a5bd835aab6f742
joenelsong <joenelsong@gmail.com>
2015-11-14 17:42:09 -0800

    commit

--- a4/findCustState.php
+++ a4/findCustState.php
@@ -71,9 +71,8 @@ if (!$stmt->bind_result($out_description, $out_revenues)) {
 }
 
 while ($stmt->fetch()) {
-    printf("%20s %12s", $out_description, $out_revenues);
+    printf("%20s %12s\r\n", $out_description, $out_revenues);
     //print PHP_EOL;
-    printf("\r\n"); // new line and carriage return
 }
 //$result = mysqli_query($conn, $query)
 //or die(mysqli_error($conn));
